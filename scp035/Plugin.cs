f4226b67a4fe75ef93a75c51b28a1d333cd9c92f
Jack Fisherman <Infernogaming621@gmail.com>
2019-05-20 23:53:01 -0400

    version number again moonkaees

--- scp035/Plugin.cs
+++ scp035/Plugin.cs
@@ -7,7 +7,7 @@ namespace scp035
 	name = "SCP-035",
 	description = "Adds SCP-035 to the game.",
 	id = "cyan.scp035",
-	version = "1.4.0",
+	version = "1.5.0",
 	SmodMajor = 3,
 	SmodMinor = 0,
 	SmodRevision = 0
