99a0b1b013d6e9bdb50dbf40175b8e7a60c6cb6a
Cyanox <Infernogaming621@gmail.com>
2020-02-08 15:48:10 -0500

    Updated default possible items

--- scp035/Configs.cs
+++ scp035/Configs.cs
@@ -46,7 +46,7 @@ namespace scp035
 			Configs.corrodeHostAmount = Plugin.Config.GetInt("035_corrode_host_amount", 5);
 			if (Configs.possibleItems == null || Configs.possibleItems.Count == 0)
 			{
-				Configs.possibleItems = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 19, 20, 21, 23, 24, 25, 26, 30 };
+				Configs.possibleItems = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 19, 20, 21, 23, 24, 25, 26, 27, 30, 33, 34 };
 			}
 		}
 	}
